0a31ed8dff16aeaa583098fdaeef8058ac6ad957
Giacomo <giacomo.tontini28@gmail.com>
2021-06-22 11:33:54 +0200

    Change: first working NodeDiscovery service

--- controllers/Default.js
+++ controllers/Default.js
@@ -1,11 +1,16 @@
 'use strict';
+const activeUeHandler = require('./handlers/DefaultHandler')
 
 module.exports.getActiveUEs = function getActiveUEs (req, res, next) {
+    res.status(200).send(activeUeHandler.retrieveAllUEendpoint())
 };
 
-module.exports.unregisterUE = function unregisterUE (req, res, next, imsi) {
+module.exports.unregisterUE = function unregisterUE (req, res, next) {
+    activeUeHandler.invalidateUEendpoint(req.params.supi)
+    res.status(200).send('OK')
 };
 
-module.exports.registerOrUpdateUE = function registerOrUpdateUE (req, res, next, body) {
-
+module.exports.registerOrUpdateUE = function registerOrUpdateUE (req, res, next) {
+    activeUeHandler.addUEendpoint(req.body.supi, req.body.ip, req.body.port)
+    res.status(200).send('OK')
 };
--- controllers/handlers/DefaultHandler.js
+++ controllers/handlers/DefaultHandler.js
@@ -1,1 +1,19 @@
-//TODO: implement
+const store = require('store')
+let expirePlugin = require('store/plugins/expire')
+store.addPlugin(expirePlugin)
+
+module.exports.addUEendpoint = (supi, ip, port) => {
+    store.set(supi, {ip: ip, port: port})
+}
+
+module.exports.invalidateUEendpoint = (supi) => {
+    store.remove(supi)
+}
+
+module.exports.retrieveAllUEendpoint = () => {
+    const res = []
+    store.each((value, key) => {
+       res.push({supi: key, ip: value.ip, port: value.port})
+    })
+    return res
+}

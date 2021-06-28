a0af5b5009940734cd2c985c3e245dfa7934be8d
Giacomo <giacomo.tontini28@gmail.com>
2021-06-28 15:23:45 +0200

    Change ttl implementation

--- controllers/handlers/DefaultHandler.js
+++ controllers/handlers/DefaultHandler.js
@@ -1,24 +1,32 @@
-const store = require('store')
-let expirePlugin = require('store/plugins/expire')
-store.addPlugin(expirePlugin)
-
-const expirationInterval = 30000
+const wss = require('../../index') 
+var CachemanMemory = require('recacheman-memory');
+var cache = new CachemanMemory();
+const ttl = 30
 
 module.exports.addUEendpoint = (supi, ip, port) => {
-    var expiration = new Date().getTime() + expirationInterval
-    store.set(supi, {ip: ip, port: port}, expiration)
+    cache.set(supi, {ip: ip, port: port}, ttl, function (err, value) {
+        if (err) throw err;
+        console.log('expired:', value);
+        wss.clients.forEach(element => {
+            wss.clients.forEach((client) => { client.send(JSON.stringify({supi: supi, event: 'ue_expired'})) });
+        });
+    })
 }
 
 module.exports.invalidateUEendpoint = (supi) => {
-    store.remove(supi)
+    cache.del(supi, function (err) {
+        if (err) throw err;
+        console.log('removed:', supi)
+      });
 }
 
 module.exports.retrieveAllUEendpoint = () => {
-    store.removeExpiredKeys()
     const res = []
-    store.each((value, key) => {
-        if(!key.includes('expire_mixin')) 
-            res.push({supi: key, ip: value.ip, port: value.port})
+    cache.getAll(function (err, data) {
+        if (err) throw err;
+        data.forEach(obj => {
+            res.push({supi: obj.key, ip: obj.data.ip, port: obj.data.port})
+        })
     })
     return res
 }

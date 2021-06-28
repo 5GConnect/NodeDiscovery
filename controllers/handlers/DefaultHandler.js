7fe3346d4a542c0d75f3344e36cd652a75ec1513
Giacomo <giacomo.tontini28@gmail.com>
2021-06-28 11:18:43 +0200

    add endpoint reachability expiration

--- controllers/handlers/DefaultHandler.js
+++ controllers/handlers/DefaultHandler.js
@@ -2,8 +2,11 @@ const store = require('store')
 let expirePlugin = require('store/plugins/expire')
 store.addPlugin(expirePlugin)
 
+const expirationInterval = 30000
+
 module.exports.addUEendpoint = (supi, ip, port) => {
-    store.set(supi, {ip: ip, port: port})
+    var expiration = new Date().getTime() + expirationInterval
+    store.set(supi, {ip: ip, port: port}, expiration)
 }
 
 module.exports.invalidateUEendpoint = (supi) => {
@@ -11,9 +14,11 @@ module.exports.invalidateUEendpoint = (supi) => {
 }
 
 module.exports.retrieveAllUEendpoint = () => {
+    store.removeExpiredKeys()
     const res = []
     store.each((value, key) => {
-       res.push({supi: key, ip: value.ip, port: value.port})
+        if(!key.includes('expire_mixin')) 
+            res.push({supi: key, ip: value.ip, port: value.port})
     })
     return res
 }
